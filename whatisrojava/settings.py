cae9504c21b2c72a8a275016c4ce6aeb2c9a8148
Ben Tarnoff <ben@bentarnoff.com>
2015-11-02 23:04:56 -0800

    static files

--- whatisrojava/settings.py
+++ whatisrojava/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 #STATIC_URL = '/static/'
 
