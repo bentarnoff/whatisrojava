aa8746119735cf51c2f411f0cc1cfde184d7a970
Ben Tarnoff <ben@bentarnoff.com>
2015-11-02 22:15:55 -0800

    fixin

--- whatisrojava/settings.py
+++ whatisrojava/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY3')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 #Email server info
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
